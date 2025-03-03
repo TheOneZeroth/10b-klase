265ab61620c6ca6f8bc9fb3c3021c07908c4ff9e
ernests1134 <ernests.samorukovs@gmail.com>
2025-03-03 11:05:58 +0200

    03.03.2025

---
+++ 2 Semestris/03.03.2025/script2.js
@@ -0,0 +1,11 @@
+let masivs1 = [1,2,3,4,5]
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i)
+}
+
+let masivs2 = [4,6,9,2,1,6]
+
+for (let i = 0; i <= length; i++) {
+    console.log(masivs2[0]+[1]+[2]+[3]+[4]+[5]+[6])
+}
